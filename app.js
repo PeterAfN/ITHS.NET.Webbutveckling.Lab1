e7539ef65df2ece10a5ba4b06a3adde16bc17b09
peter palosaari <peterpalosaari@live.se>
2021-03-29 02:30:19 +0200

    create table for data

--- app.js
+++ app.js
@@ -1,26 +1,137 @@
-// const listCoursesButton = document.querySelector('#listCourses');
+"use strict";
 
-// const { json } = require("body-parser");
-// const getClassroomCourses = {};
+// const classroom = document.querySelector(".container-classroomcourses");
+// const ondemand = document.querySelector(".container-ondemandcourses");
 
-// function getData()  {
-//   fetch('http://localhost:3000/klassrumskurser')
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
+// getClassroomCoursesData();
+// getOnDemandCoursesData();
+
+// function getClassroomCoursesData() {
+//   fetch("http://localhost:3000/klassrumskurser")
+//     .then((response) => response.json())
+//     .then((data) => printHTMLClassroomCourses(data));
 // }
 
+// function getOnDemandCoursesData() {
+//   fetch("http://localhost:3000/ondemandkurser")
+//     .then((response) => response.json())
+//     .then((data) => printHTMLOnDemandCourses(data));
+// }
 
-const listCoursesApi = function(){
-    //Hämta data ur courses.json filen
-    fetch('http://localhost:3000/klassrumskurser')
-      //När anropet är klart och data finns.
-      .then((response) => response.json())
-      .then((data) => writeToConsole(data));
-  }
+// function printHTMLClassroomCourses(indata) {
+//   indata.forEach((course) => {
+//     classroom.insertAdjacentHTML(
+//       "beforeend",
+//       `
+//       <div class="box" id="box-1">
+//         <ul>
+//             <li class="course-list-item-1">Kurs: ${course.title}</li>
+//             <li>Beskrivning: ${course.description}</li>
+//             <li>Kategori: ${course.category}</li>
+//             <li>Längd: ${course.length} h</li>
+//             <li>Kurstyp: Klassrumsundervisning</li>
+//             <li>Kursnummer: ${course.courseNumber}</li>
+//         </ul>
+//       </div>
+//       `
+//     );
+//   });
+// }
 
-  function writeToConsole(indata) {
-    console.log(indata)
+// function printHTMLOnDemandCourses(indata) {
+//   indata.forEach((course) => {
+//     ondemand.insertAdjacentHTML(
+//       "beforeend",
+//       `
+//           <div class="box" id="box-1">
+//             <ul>
+//                 <li class="course-list-item-1">Kurs:${course.title}</li>
+//                 <li>Beskrivning: ${course.description}</li>
+//                 <li>Kategori: ${course.category}</li>
+//                 <li>Längd: ${course.length} h</li>
+//                 <li>Kurstyp: On-Demand, nedladdningsbar</li>
+//                 <li>Kursnummer: ${course.courseNumber}</li>
+//             </ul>
+//           </div>
+//           `
+//     );
+//   });
+// }
+
+const coursesTableContent = document.querySelector('#courses-table-content');
+const tableView = document.querySelector('#tableView');
+// const addVehicleView = document.querySelector('#addVehicle-container');
+// const searchInput = document.querySelector('#searchInput');
+// const searchButton = document.querySelector('#searchButton');
+// const saveButton = document.querySelector('#save');
+// const regNoInput = document.querySelector('#regNoInput');
+// const makeInput = document.querySelector('#makeInput');
+// const modelInput = document.querySelector('#modelInput');
+// const modelYearInput = document.querySelector('#modelYearInput');
+// const mileageInput = document.querySelector('#mileageInput');
+// const valueInput = document.querySelector('#valueInput');
+// const addNewButton = document.querySelector('#addNewVehicle');
+
+getClassroomCoursesData();
+getOnDemandCoursesData();
+
+function getClassroomCoursesData() {
+  fetch("http://localhost:3000/klassrumskurser")
+    .then((response) => response.json())
+    .then((data) => createClassroomCoursesTable(data));
+}
+
+function getOnDemandCoursesData() {
+  fetch("http://localhost:3000/ondemandkurser")
+    .then((response) => response.json())
+    .then((data) => createOnDemandCoursesTable(data));
 }
 
 
-listCoursesApi();
+function createClassroomCoursesTable(courses){
+  coursesTableContent.innerHTML = '';
+  for (let course of courses) {
+    createClassroomCoursesRow(course);
+  }
+}
+
+function createOnDemandCoursesTable(courses){
+  coursesTableContent.innerHTML = '';
+  for (let course of courses) {
+    createOnDemandCoursesRow(course);
+  }
+}
+
+function createClassroomCoursesRow(course){
+  coursesTableContent.insertAdjacentHTML(
+    'beforeend',
+    `
+        <tr>
+          <td>${course.id}</td>
+          <td>${course.title}</td>
+          <td>${course.description}</td>
+          <td>${course.category}</td>
+          <td>${course.length}</td>
+          <td>Klassrumskurs</td>
+          <td>${course.courseNumber}</td>
+        </tr>
+      `
+  );
+}
+
+function createOnDemandCoursesRow(course){
+  coursesTableContent.insertAdjacentHTML(
+    'beforeend',
+    `
+        <tr>
+          <td>${course.id}</td>
+          <td>${course.title}</td>
+          <td>${course.description}</td>
+          <td>${course.category}</td>
+          <td>${course.length}</td>
+          <td>On-Demand</td>
+          <td>${course.courseNumber}</td>
+        </tr>
+      `
+  );
+}
